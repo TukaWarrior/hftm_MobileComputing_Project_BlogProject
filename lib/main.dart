cfbadfd058740a7f0a8344a49ed32706a692a80d
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-06-24 23:54:45 +0200

    Changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,10 @@ class MainApp extends StatelessWidget {
       providers: [createBlogProvider()],
       child: MaterialApp(
         navigatorKey: mainNavigatorKey,
-        title: "Interaction and State",
+        title: "Lucas Blog",
         theme: theme,
         home: const MainScreen(),
       ),
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:blog_project/view/home_screen.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Blog Project',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const HomePage(title: 'Flutter Blog Home Page'),
-//     );
-//   }
-// }
