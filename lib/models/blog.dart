4c332f6101cbdaf365b4b70d8bc7c9f71128aaa5
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-09 18:03:35 +0200

    Added toJson and fromJson methods to blog class

--- lib/models/blog.dart
+++ lib/models/blog.dart
@@ -13,4 +13,26 @@ class Blog {
   });
 
   String get publishedDateString => "${publishedAt.day}.${publishedAt.month}.${publishedAt.year}";
+
+  // Factory method to create a Blog instance from JSON
+  factory Blog.fromJson(Map<String, dynamic> json) {
+    return Blog(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      publishedAt: DateTime.parse(json['publishedAt']),
+      // isLikedByMe: json['isLikedByMe'] ?? false,
+    );
+  }
+
+  // Method to convert a Blog instance to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'publishedAt': publishedAt.toIso8601String(),
+      // 'isLikedByMe': isLikedByMe,
+    };
+  }
 }
