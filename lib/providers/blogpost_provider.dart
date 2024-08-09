fa1c90c93506fdfbc4ed5c2b04477f0e2b845b86
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-10 01:08:12 +0200

    Tried to implement a new page

---
+++ lib/providers/blogpost_provider.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:blog_project/models/blogpost.dart';
+import 'package:blog_project/services/blogpost_api.dart';
+import 'package:flutter/material.dart';
+import 'package:blog_project/models/blog.dart';
+import 'package:blog_project/services/blog_repository.dart';
+
+class BlogProvider extends ChangeNotifier {
+  bool isLoading = false;
+  List<BlogPost> _blogpost = [];
+  List<BlogPost> get blogs => _blogpost;
+  final BlogService _blogService = BlogService(); // Create an instance of BlogService
+
+  BlogProvider() {
+    _startRefreshTimer();
+    readBlogsWithLoadingState();
+  }
+
+  /// Refresh every Minute
+  void _startRefreshTimer() {
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      readBlogs();
+    });
+  }
+
+  Future<void> readBlogsWithLoadingState() async {
+    isLoading = true;
+    notifyListeners();
+
+    await readBlogs(withNotifying: false);
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> readBlogs({bool withNotifying = true}) async {
+    _blogpost = await _blogService.fetchBlogs();
+    if (withNotifying) {
+      notifyListeners();
+    }
+  }
+}
