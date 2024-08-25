a069b0722109684525d11db47228baf10cbb99c4
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-26 01:30:24 +0200

    Added new font family in themes.dart

--- lib/settings/theme.dart
+++ lib/settings/theme.dart
@@ -4,6 +4,7 @@ final ThemeData theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyanAccent),
   scaffoldBackgroundColor: const ColorScheme.light().surfaceContainerHighest,
+  fontFamily: "Mulish",
   appBarTheme: AppBarTheme(
     backgroundColor: const ColorScheme.light().surfaceContainerHighest,
   ),
