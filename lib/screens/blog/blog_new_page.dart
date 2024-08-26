62b863835ea8b2215f6617b64e7852825937d204
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-27 00:49:01 +0200

    Reworked blog details page

--- lib/screens/blog/blog_new_page.dart
+++ lib/screens/blog/blog_new_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:blog_project/models/blog.dart';
@@ -128,6 +129,8 @@ class _BlogNewPageState extends State<BlogNewPage> {
       title: title,
       content: content,
       publishedAt: DateTime.now(),
+      category: BlogCategory.Technology,
+      likes: 42,
       imagePath: imagePath, // Save the image path
     ));
     blogProvider.readBlogs();
