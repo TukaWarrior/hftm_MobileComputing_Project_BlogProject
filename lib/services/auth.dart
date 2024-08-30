be90eed21cfd223d4c0069239911f499ad72b360
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-31 00:43:21 +0200

    Create auth.dart. It handles the userstream from firebase and the method to synchronously get the userstate

---
+++ lib/services/auth.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final userStream =
+      FirebaseAuth.instance.authStateChanges(); // Accessing the firebase user asynchronously through a stream. Used when e.g. the UI should react to an auth state change, but the time of when that change is going to happen is unknown.
+  final user = FirebaseAuth.instance.currentUser; // Accessing the firebase user synchronously by calling FirebaseAuth. instance. Used when e.g. the user clicks a button and the authentication state has to change in that moment.
+}
