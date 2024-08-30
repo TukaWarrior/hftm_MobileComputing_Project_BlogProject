0b6bb7c70052e73d6b0aa13dbcfca3fc8e32e54d
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-08-30 23:36:58 +0200

    Fixed accidental placement of dependencies as dev-dependencies, lowered min required android API to 33 from 35

--- lib/screens/shared/navigation_bar.dart
+++ lib/screens/shared/navigation_bar.dart
@@ -49,3 +49,36 @@ class _NavBarState extends State<NavBar> {
     );
   }
 }
+
+// @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         body: _pages[selectedIndex], // Display the selected page
+//         bottomNavigationBar: Container(
+//           height: 64.0,
+//           margin: EdgeInsets.all(24.0),
+//           child: ClipRRect(
+//             borderRadius: BorderRadius.circular(16.0),
+//             child: NavigationBar(
+//               destinations: const [
+//                 NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Home'),
+//                 NavigationDestination(icon: Icon(Icons.category_outlined), label: 'BlogView'),
+//                 NavigationDestination(icon: Icon(Icons.explore_outlined), label: 'CreateBlog'),
+//                 NavigationDestination(icon: Icon(Icons.settings_outlined), label: 'SensorData'),
+//               ],
+//               selectedIndex: selectedIndex,
+//               onDestinationSelected: (int index) {
+//                 setState(() {
+//                   selectedIndex = index;
+//                 });
+//               },
+              // indicatorColor: Colors.transparent,
+              // backgroundColor: Colors.grey.withOpacity(0.25),
+              // elevation: 0.0,
+              // labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+//               animationDuration: const Duration(milliseconds: 500),
+//             ),
+//           ),
+//         ),
+      // ),
