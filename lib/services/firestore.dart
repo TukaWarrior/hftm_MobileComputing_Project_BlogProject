f1db145e13321bc9d9f3800bfd8631b0bff51e68
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-01 18:54:21 +0200

    Added dependencies: firebase_storage, to access firebase storage used for storing images

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_blog/models/blogpost.dart';
 
 // firestore.dart handles the data fetching from the firestore database.
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   Future<List<BlogPost>> getBlogPosts() async {
     var ref = _db.collection('blogposts');
@@ -19,4 +21,16 @@ class FirestoreService {
     var snapshot = await ref.get();
     return BlogPost.fromJson(snapshot.data() ?? {});
   }
+
+  // Future<String> getDownloadURL(String filePath) async {
+  //   try {
+  //     Reference ref = _storage.ref().child(filePath);
+  //     String url = await ref.getDownloadURL();
+  //     print("Download URL: $url");
+  //     return url;
+  //   } catch (e) {
+  //     print("Failed to get the download URL: $e");
+  //     return "";
+  //   }
+  // }
 }
