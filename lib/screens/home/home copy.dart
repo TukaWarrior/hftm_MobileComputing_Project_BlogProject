29ba273be247bc147d1fb7375e7b39f738bdd1ee
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-05 01:24:01 +0200

    Implemented creation of user profile when logging in

--- lib/screens/home/home copy.dart
+++ lib/screens/home/home copy.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blog/screens/blogpost/blogpost.dart';
 import 'package:flutter_blog/screens/login/login.dart';
 import 'package:flutter_blog/services/auth.dart';
-import 'package:flutter_blog/services/profile_provider.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,14 +12,12 @@ class HomeScreen extends StatelessWidget {
       stream: AuthService().userStream,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text('Loading...');
+          return const Text('loading');
         } else if (snapshot.hasError) {
           return const Center(
             child: Text('Error'),
           );
         } else if (snapshot.hasData) {
-          // Fetch and initialize the user profile
-          Provider.of<ProfileProvider>(context, listen: false).fetchUserProfile();
           return const BlogPostScreen();
         } else {
           return const LoginScreen();
---
+++ lib/services/profile_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/models/profile.dart';
+import 'package:flutter_blog/services/firestore.dart';
+
+class ProfileProvider with ChangeNotifier {
+  Profile? _profile;
+  final FirestoreService _firestoreService = FirestoreService();
+
+  Profile? get profile => _profile;
+
+  Future<void> fetchUserProfile() async {
+    try {
+      final fetchedProfile = await _firestoreService.getProfile();
+      if (fetchedProfile != null) {
+        _profile = fetchedProfile;
+        notifyListeners(); // Notify listeners to update UI when profile data changes
+      } else {
+        // If the profile doesn't exist, create a new one
+        final newProfile = Profile(
+          displayName: 'New User',
+          description: 'New Description',
+          email: _firestoreService.user!.email ?? '', // Or get email from auth service
+          avatarURL: '',
+          createdDate: DateTime.now(),
+        );
+        await _firestoreService.createOrUpdateProfile(newProfile);
+        _profile = newProfile;
+        notifyListeners();
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch or create user profile: $e');
+    }
+  }
+
+  Future<void> updateUserProfile(Profile updatedProfile) async {
+    try {
+      await _firestoreService.createOrUpdateProfile(updatedProfile);
+      _profile = updatedProfile;
+      notifyListeners(); // Notify listeners to update UI when profile data changes
+    } catch (e) {
+      throw Exception('Failed to update user profile: $e');
+    }
+  }
+}
