574936b555f5bee86db29d418a4729051057b898
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-09-06 22:24:42 +0200

    Category entities now have a documentID field that corresponds to the documentid from firebase.  This is necessarty for delete or edit operation to fetch / delete the correct entity.

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,16 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'category.g.dart';
 
 @JsonSerializable()
 class Category {
+  final String documentID;
   final String name;
   final String emoji;
 
   Category({
+    this.documentID = '',
     this.name = '',
     this.emoji = '',
   });
+
+  // Constructor to create Category from Firestore DocumentSnapshot
+  factory Category.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Category(
+      documentID: doc.id,
+      name: data['name'] ?? '',
+      emoji: data['emoji'] ?? '',
+    );
+  }
 
   factory Category.fromJson(Map<String, dynamic> json) => _$CategoryFromJson(json);
   Map<String, dynamic> toJson() => _$CategoryToJson(this);
--- lib/models/category.g.dart
+++ lib/models/category.g.dart
@@ -7,11 +7,13 @@ part of 'category.dart';
 // **************************************************************************
 
 Category _$CategoryFromJson(Map<String, dynamic> json) => Category(
+      documentID: json['documentID'] as String? ?? '',
       name: json['name'] as String? ?? '',
       emoji: json['emoji'] as String? ?? '',
     );
 
 Map<String, dynamic> _$CategoryToJson(Category instance) => <String, dynamic>{
+      'documentID': instance.documentID,
       'name': instance.name,
       'emoji': instance.emoji,
     };
